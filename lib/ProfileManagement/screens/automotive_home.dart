ddd81e83a9096c31bbe077ee4bef18cb613b81a0
PaulVincentLerado17 <104497476+PaulVincentLerado17@users.noreply.github.com>
2024-09-06 15:22:33 +0800

    update: home appbar

--- lib/ProfileManagement/screens/automotive_home.dart
+++ lib/ProfileManagement/screens/automotive_home.dart
@@ -11,7 +11,8 @@ class _AutomotiveHomeState extends State<AutomotiveHome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Home'),),
+      backgroundColor: Colors.grey.shade300,
+      appBar: AppBar(title: Text('Home', style: TextStyle(fontWeight: FontWeight.w900),), backgroundColor: Colors.grey.shade300,),
     );
   }
 }
