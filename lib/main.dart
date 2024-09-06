050bbc4de34736f22178ced7fc24885b648e5581
PaulVincentLerado17 <104497476+PaulVincentLerado17@users.noreply.github.com>
2024-09-06 15:13:03 +0800

    update: connected to main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:autocare_automotiveshops/ProfileManagement/widgets/navbar.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,19 +11,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'AutoCare Automotive Shops',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('AutoCare Automotive Shops'),
-        ),
-        body: Center(
-          child: Text('Welcome to AutoCare Automotive Shops!'),
-        ),
-      ),
+    return
+        MaterialApp(
+            debugShowCheckedModeBanner: false,
+            home: Navbar()
     );
   }
 }
