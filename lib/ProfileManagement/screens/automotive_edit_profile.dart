c24deea6d14aeac229dd252f06ff909779329cae
PaulVincentLerado17 <104497476+PaulVincentLerado17@users.noreply.github.com>
2024-09-06 21:10:53 +0800

    update: edit profile initial page

---
+++ lib/ProfileManagement/screens/automotive_edit_profile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AutomotiveEditProfile extends StatefulWidget {
+  const AutomotiveEditProfile({super.key});
+
+  @override
+  State<AutomotiveEditProfile> createState() => _AutomotiveEditProfileState();
+}
+
+class _AutomotiveEditProfileState extends State<AutomotiveEditProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
