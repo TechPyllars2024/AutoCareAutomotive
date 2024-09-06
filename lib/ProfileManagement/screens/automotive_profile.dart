959006094b3f1a4f15690e0fac53b2fc583e5ae3
PaulVincentLerado17 <104497476+PaulVincentLerado17@users.noreply.github.com>
2024-09-06 19:13:56 +0800

    update: profile and cover photo and name

--- lib/ProfileManagement/screens/automotive_profile.dart
+++ lib/ProfileManagement/screens/automotive_profile.dart
@@ -8,47 +8,78 @@ class AutomotiveProfile extends StatefulWidget {
 }
 
 class _AutomotiveProfileState extends State<AutomotiveProfile> {
+  final double coverHeight = 220;
+  final double profileHeight = 130;
+
   @override
   Widget build(BuildContext context) {
+    final double top = coverHeight - profileHeight / 2;
+
     return Scaffold(
       backgroundColor: Colors.grey.shade300,
-      appBar: AppBar(title: Text('Profile', style: TextStyle(fontWeight: FontWeight.w900),), backgroundColor: Colors.grey.shade300,),
-
-      body: Stack(
-        clipBehavior:Clip.none,
-        alignment: Alignment.center,
+      appBar: AppBar(
+        title: Text(
+          'Profile',
+          style: TextStyle(fontWeight: FontWeight.w900),
+        ),
+        backgroundColor: Colors.grey.shade300,
+        foregroundColor: Colors.black, // Ensures text is visible on AppBar
+      ),
+      body: ListView(
+        padding: EdgeInsets.zero,
         children: [
-          buildCoverImage(),
-          Positioned(
-            left: 20,
-              bottom: -60,
-
-              child: buildProfileImage())
+          buildTopSection(top),
+          buildContent(),
         ],
-      )
-
+      ),
     );
   }
 
+  Widget buildContent() => Padding(
+    padding: const EdgeInsets.all(16.0), // Add padding to the container
+    child: Align(
+      alignment: Alignment.centerLeft,
+      child: Text(
+        'Auto Repair',
+        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      ),
+    ),
+  );
+
+
+  Widget buildTopSection(double top) {
+    return Stack(
+      clipBehavior: Clip.none,
+      alignment: Alignment.center,
+      children: [
+        Container(
+          margin: EdgeInsets.only(bottom: profileHeight / 2),
+          child: buildCoverImage(),
+        ),
+        Positioned(
+          left: 20,
+          top: top,
+          child: buildProfileImage(),
+        ),
+      ],
+    );
+  }
+
+  Widget buildCoverImage() => Container(
+    color: Colors.grey,
+    child: Image.network(
+      'https://www.erieinsurance.com/-/media/images/blog/articlephotos/2018/rentalcarlg.ashx?h=529&w=1100&la=en&hash=B6312A1CFBB03D75789956B399BF6B91E7980061',
+      width: double.infinity,
+      height: coverHeight,
+      fit: BoxFit.cover,
+    ),
+  );
+
+  Widget buildProfileImage() => CircleAvatar(
+    radius: profileHeight / 2,
+    backgroundColor: Colors.grey.shade800,
+    backgroundImage: NetworkImage(
+      'https://cdn.vectorstock.com/i/500p/57/48/auto-repair-service-logo-badge-emblem-template-vector-49765748.jpg',
+    ),
+  );
 }
-
-Widget buildCoverImage() => Container(
-  color: Colors.grey,
-  child: Image.network('https://www.erieinsurance.com/-/media/images/blog/articlephotos/2018/rentalcarlg.ashx?h=529&w=1100&la=en&hash=B6312A1CFBB03D75789956B399BF6B91E7980061',
-    width: double.infinity,
-    height: 250,
-  fit: BoxFit.cover,
-  ),
-
-
-);
-
-Widget buildProfileImage() => CircleAvatar(
-  radius: 70,
-  backgroundColor: Colors.grey.shade800,
-  backgroundImage: NetworkImage(
-      'https://cdn.vectorstock.com/i/500p/57/48/auto-repair-service-logo-badge-emblem-template-vector-49765748.jpg'
-  ),
-);
-
-
