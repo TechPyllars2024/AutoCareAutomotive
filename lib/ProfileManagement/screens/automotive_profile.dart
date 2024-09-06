22fcc7689cd661181881accbeee2a9c19dd060dd
PaulVincentLerado17 <104497476+PaulVincentLerado17@users.noreply.github.com>
2024-09-06 15:56:32 +0800

    update: profile photo and cover photo

--- lib/ProfileManagement/screens/automotive_profile.dart
+++ lib/ProfileManagement/screens/automotive_profile.dart
@@ -13,6 +13,42 @@ class _AutomotiveProfileState extends State<AutomotiveProfile> {
     return Scaffold(
       backgroundColor: Colors.grey.shade300,
       appBar: AppBar(title: Text('Profile', style: TextStyle(fontWeight: FontWeight.w900),), backgroundColor: Colors.grey.shade300,),
+
+      body: Stack(
+        clipBehavior:Clip.none,
+        alignment: Alignment.center,
+        children: [
+          buildCoverImage(),
+          Positioned(
+            left: 20,
+              bottom: -60,
+
+              child: buildProfileImage())
+        ],
+      )
+
     );
   }
+
 }
+
+Widget buildCoverImage() => Container(
+  color: Colors.grey,
+  child: Image.network('https://www.erieinsurance.com/-/media/images/blog/articlephotos/2018/rentalcarlg.ashx?h=529&w=1100&la=en&hash=B6312A1CFBB03D75789956B399BF6B91E7980061',
+    width: double.infinity,
+    height: 250,
+  fit: BoxFit.cover,
+  ),
+
+
+);
+
+Widget buildProfileImage() => CircleAvatar(
+  radius: 70,
+  backgroundColor: Colors.grey.shade800,
+  backgroundImage: NetworkImage(
+      'https://cdn.vectorstock.com/i/500p/57/48/auto-repair-service-logo-badge-emblem-template-vector-49765748.jpg'
+  ),
+);
+
+
