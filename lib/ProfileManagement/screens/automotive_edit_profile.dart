79fd66924f9297f12359f8ceecb6554c58f2ce81
PaulVincentLerado17 <104497476+PaulVincentLerado17@users.noreply.github.com>
2024-09-07 23:58:08 +0800

    update: edit page appbar

--- lib/ProfileManagement/screens/automotive_edit_profile.dart
+++ lib/ProfileManagement/screens/automotive_edit_profile.dart
@@ -10,6 +10,8 @@ class AutomotiveEditProfile extends StatefulWidget {
 class _AutomotiveEditProfileState extends State<AutomotiveEditProfile> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text('Edit Profile'),),
+    );
   }
 }
