f67ffc5ba12f1bae62bc3a1d5486ab56e0fdc7e7
PaulVincentLerado17 <104497476+PaulVincentLerado17@users.noreply.github.com>
2024-09-11 20:46:24 +0800

    update: edit profile styles

--- lib/ProfileManagement/screens/automotive_edit_profile.dart
+++ lib/ProfileManagement/screens/automotive_edit_profile.dart
@@ -368,7 +368,7 @@ class _AutomotiveEditProfileState extends State<AutomotiveEditProfile> {
                             vertical: 15.0, horizontal: 40),
                         decoration: BoxDecoration(
                           color: Colors.white,
-                          borderRadius: BorderRadius.circular(10),
+                          borderRadius: BorderRadius.circular(15),
                           border:
                               Border.all(color: Colors.grey), // Border color
                         ),
@@ -382,7 +382,7 @@ class _AutomotiveEditProfileState extends State<AutomotiveEditProfile> {
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      const Text('Close'),
+                      const Text('Save'),
                       const SizedBox(height: 5),
                       // Add some space between the text and container
                       Container(
@@ -390,7 +390,7 @@ class _AutomotiveEditProfileState extends State<AutomotiveEditProfile> {
                             vertical: 15.0, horizontal: 40),
                         decoration: BoxDecoration(
                           color: Colors.white,
-                          borderRadius: BorderRadius.circular(10),
+                          borderRadius: BorderRadius.circular(15),
                           border:
                               Border.all(color: Colors.grey), // Border color
                         ),
@@ -436,35 +436,39 @@ class _AutomotiveEditProfileState extends State<AutomotiveEditProfile> {
         ),
       );
 
+
+
   Widget dayOfTheWeekSelection() => Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              'Select Days of the Week',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            DayOfTheWeek(
-              options: const [
-                'Monday',
-                'Tuesday',
-                'Wednesday',
-                'Thursday',
-                'Friday',
-                'Saturday',
-                'Sunday'
-              ],
-              hintText: 'Select Days',
-              controller: daysOfTheWeekController,
-              onSelectionChanged: (selectedOptions) {
-                print('Selected Options: $selectedOptions');
-              },
-            ),
-          ],
+    padding: const EdgeInsets.all(8.0),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
+          'Select Days of the Week',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
         ),
-      );
+        DayOfTheWeek(
+          options: const [
+            'Monday',
+            'Tuesday',
+            'Wednesday',
+            'Thursday',
+            'Friday',
+            'Saturday',
+            'Sunday'
+          ],
+          hintText: 'Select Days',
+          controller: daysOfTheWeekController,
+          onSelectionChanged: (selectedOptions) {
+            print('Selected Options: $selectedOptions');
+          },
+
+        ),
+      ],
+    ),
+  );
+
 }
