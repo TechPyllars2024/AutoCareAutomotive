fde90322f9532c02defc7cb312d318248d11a85d
PaulVincentLerado17 <104497476+PaulVincentLerado17@users.noreply.github.com>
2024-09-11 10:06:59 +0800

    update: addded edit profile input style

--- lib/ProfileManagement/screens/automotive_edit_profile.dart
+++ lib/ProfileManagement/screens/automotive_edit_profile.dart
@@ -1,5 +1,10 @@
 
 import 'package:autocare_automotiveshops/ProfileManagement/widgets/text_field.dart';
+import 'package:autocare_automotiveshops/ProfileManagement/widgets/timeSelection.dart';
+import 'package:autocare_automotiveshops/ProfileManagement/widgets/dropdown.dart';
+import 'package:get/get.dart'; // Import GetX for state management
+
+
 import 'package:flutter/material.dart';
 
 
@@ -11,6 +16,11 @@ class AutomotiveEditProfile extends StatefulWidget {
 }
 
 class _AutomotiveEditProfileState extends State<AutomotiveEditProfile> {
+  final DropdownController controller = Get.put(DropdownController());
+
+
+  TimeOfDay _timeOfDay = TimeOfDay.now();
+
   final double coverHeight = 220;
   final double profileHeight = 130;
 
@@ -44,11 +54,9 @@ class _AutomotiveEditProfileState extends State<AutomotiveEditProfile> {
               buildTopSection(top),
               const SizedBox(height: 20),
               buildInputs(),
-
+              TimeSelection(),
               servicesCarousel(),
-
-
-
+              ServiceSpecialization(),
               //ServicesSection(),
             ],
           ),
@@ -345,6 +353,82 @@ class _AutomotiveEditProfileState extends State<AutomotiveEditProfile> {
         ),
       ),
     ],
+  );
+
+
+
+  Widget TimeSelection() => Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Operating hours',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(height: 10),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text('Open'),
+                  SizedBox(height: 5), // Add some space between the text and container
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 40),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(color: Colors.grey), // Border color
+                    ),
+                    child: TimePickerDisplay(initialTime: TimeOfDay.now()),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(width: 15),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text('Close'),
+                  SizedBox(height: 5), // Add some space between the text and container
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 40),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(color: Colors.grey), // Border color
+                    ),
+                    child: TimePickerDisplay(initialTime: TimeOfDay.now()),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ],
+    ),
+  );
+
+
+
+  Widget ServiceSpecialization() => Container(
+    child: CustomDropdown(
+      options: ['Electrical Works', 'Mechanical Works', 'Air-conditioninf', 'Paint and Body Works', 'Car Wash and Auto-Detailing'],
+      hintText: 'Service Specialization',
+      controller: controller,
+      onSelectionChanged: (selectedOptions) {
+        print('Selected Options: $selectedOptions');
+      },
+    ),
+
   );
 
 
