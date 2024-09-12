72f57828aaa1d33cae8f8717927b0f24d164a93a
PaulVincentLerado17 <104497476+PaulVincentLerado17@users.noreply.github.com>
2024-09-12 13:57:28 +0800

    update: revert pushed commits

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:autocare_automotiveshops/ProfileManagement/widgets/navbar.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,21 +8,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'AutoCare Automotive Shops',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('AutoCare Automotive Shops'),
-        ),
-        body: Center(
-          child: Text('Welcome to AutoCare Automotive Shops!'),
-        ),
+        body: Navbar(),
       ),
     );
   }
