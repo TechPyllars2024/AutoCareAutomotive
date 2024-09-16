bacc9eba5a3ff7011aa4bc47fc1334dcd414c7e6
RafaelIIIPrudente <rflprdnt@gmail.com>
2024-09-16 22:58:58 +0800

    feat manage services: created the category list

---
+++ lib/Service Management/models/category_list.dart
@@ -0,0 +1,12 @@
+class CategoryList {
+  // Static list of categories
+  static const List<String> categories = [
+    'Electrical Works',
+    'Mechanical Works',
+    'Paint and Body Works',
+    'Air-conditioning Works',
+    'Installation of Accessories',
+    'Car Wash',
+    'Auto Detailing Services',
+  ];
+}
