9d12d2cd8e17c1073e07327b1173537a4f6c0dd1
RafaelIIIPrudente <rflprdnt@gmail.com>
2024-09-18 18:57:35 +0800

    feat manage services: updated the navbar.dart

--- lib/Navigation Bar/navbar.dart
+++ lib/Navigation Bar/navbar.dart
@@ -1,5 +1,5 @@
 import 'package:autocare_automotiveshops/Booking%20Mangement/screens/automotive_booking.dart';
-import 'package:autocare_automotiveshops/ProfileManagement/screens/automotive_edit_profile.dart';
+import 'package:autocare_automotiveshops/Messages%20Management/screens/automotive_messages.dart';
 import 'package:autocare_automotiveshops/ProfileManagement/screens/automotive_profile.dart';
 import 'package:autocare_automotiveshops/Service%20Management/screens/manage_services.dart';
 import 'package:flutter/material.dart';
@@ -21,9 +21,9 @@ class _NavbarState extends State<Navbar> {
 
   final List<Widget> _screens = [
     const AutomotiveBookingScreen(),
+    const AutomotiveMessagesScreen(),
     const ServiceManagementScreen(),
     const AutomotiveProfileScreen(),
-    const AutomotiveEditProfile(),
   ];
 
   @override
@@ -34,8 +34,8 @@ class _NavbarState extends State<Navbar> {
 
         key: _bottomNavigationKey,
         items: <Widget>[
-          Icon(Icons.home, size: 35, color: _page == 0 ? Colors.orange : Colors.black),
-          Icon(Icons.calendar_month, size: 35, color: _page == 1 ? Colors.orange : Colors.black),
+          Icon(Icons.calendar_month, size: 35, color: _page == 0 ? Colors.orange : Colors.black),
+          Icon(Icons.message, size: 35, color: _page == 1 ? Colors.orange : Colors.black),
           Icon(Icons.directions_car, size: 35, color: _page == 2 ? Colors.orange : Colors.black),
           Icon(Icons.person, size: 35, color: _page == 3 ? Colors.orange : Colors.black),
         ],
