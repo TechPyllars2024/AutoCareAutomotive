d6a28f7dd64b8280b5e895af656973703b48709e
PaulVincentLerado17 <104497476+PaulVincentLerado17@users.noreply.github.com>
2024-09-19 01:30:28 +0800

    update: booking tab underdetails

--- lib/Booking Mangement/screens/automotive_booking.dart
+++ lib/Booking Mangement/screens/automotive_booking.dart
@@ -23,17 +23,37 @@ class _AutomotiveBookingState extends State<AutomotiveBookingScreen> {
           ),
           backgroundColor: Colors.grey.shade300,
           bottom: TabBar(
-            indicatorColor: Colors.blue, // Customize the tab indicator color
+            indicatorColor: Colors.orange, // Customize the tab indicator color
+            labelColor: Colors.orange, // Color of the selected tab
+            unselectedLabelColor: Colors.black, // Color of the unselected tab
             tabs: const [
-              Tab(text: 'Upcoming'),
-              Tab(text: 'History'),
+              Tab(child: Text('Pending', style: TextStyle(fontSize: 18))),
+              Tab(child: Text('History', style: TextStyle(fontSize: 18))),
             ],
           ),
         ),
         body: const TabBarView(
           children: [
             // The first tab content
-            Center(child: Text('Upcoming Bookings')),
+            Padding(
+              padding: EdgeInsets.all(16.0), // Adjust padding as needed
+              child: Column(
+                children: [
+                  Align(
+                    alignment: Alignment.topLeft, // Align text to the left
+                    child: Text(
+                      'May 17, 2025',
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                    ),
+                  ),
+                  Row(
+                    
+                    children: [Text('data')],
+                  ),
+                ],
+              ),
+              
+            ),
             // The second tab content
             Center(child: Text('Booking History')),
           ],
