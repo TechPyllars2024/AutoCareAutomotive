d0226f24e845c952a7bebacc62ebac884479106f
PaulVincentLerado17 <104497476+PaulVincentLerado17@users.noreply.github.com>
2024-09-19 01:10:51 +0800

    update: booking tabs

--- lib/Booking Mangement/screens/automotive_booking.dart
+++ lib/Booking Mangement/screens/automotive_booking.dart
@@ -12,16 +12,33 @@ class AutomotiveBookingScreen extends StatefulWidget {
 class _AutomotiveBookingState extends State<AutomotiveBookingScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey.shade300,
-      appBar: AppBar(
-        title: const Text(
-          'Bookings',
-          style: TextStyle(fontWeight: FontWeight.w900),
-        ),
+    return DefaultTabController(
+      length: 2, // Number of tabs
+      child: Scaffold(
         backgroundColor: Colors.grey.shade300,
+        appBar: AppBar(
+          title: const Text(
+            'Bookings',
+            style: TextStyle(fontWeight: FontWeight.w900),
+          ),
+          backgroundColor: Colors.grey.shade300,
+          bottom: TabBar(
+            indicatorColor: Colors.blue, // Customize the tab indicator color
+            tabs: const [
+              Tab(text: 'Upcoming'),
+              Tab(text: 'History'),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: [
+            // The first tab content
+            Center(child: Text('Upcoming Bookings')),
+            // The second tab content
+            Center(child: Text('Booking History')),
+          ],
+        ),
       ),
     );
   }
 }
-//
