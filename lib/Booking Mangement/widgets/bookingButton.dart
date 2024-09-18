2da35e42c463bdd640c7acd3f030757f73c8372f
PaulVincentLerado17 <104497476+PaulVincentLerado17@users.noreply.github.com>
2024-09-19 02:28:38 +0800

    update: adaptive button

--- lib/Booking Mangement/widgets/bookingButton.dart
+++ lib/Booking Mangement/widgets/bookingButton.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
 
-class AcceptButton extends StatelessWidget {
+class BookingButton extends StatelessWidget {
   final VoidCallback? onTap; // Nullable onTap callback
   final String text;
   final Color color;
   final double padding;
+  final double width; // Add a width parameter
 
-  const AcceptButton({
+  const BookingButton({
     super.key,
     this.onTap, // Optional onTap callback
     required this.text,
     this.color = Colors.orange,
     this.padding = 20.0,
+    this.width = 150.0, // Default width value
   });
 
   @override
@@ -24,6 +26,7 @@ class AcceptButton extends StatelessWidget {
         child: Container(
           alignment: Alignment.center,
           padding: const EdgeInsets.symmetric(vertical: 12),
+          width: width, // Apply width here
           decoration: ShapeDecoration(
             shape: const RoundedRectangleBorder(
               borderRadius: BorderRadius.all(
