d3e44ee5e47b3be50d22d3d0afaf7e8ef804627a
RafaelIIIPrudente <rflprdnt@gmail.com>
2024-09-19 17:32:53 +0800

    feature_profile_management: updated the automotive_shop_edit_profile_services.dart

--- lib/ProfileManagement/models/automotive_shop_profile_model.dart
+++ lib/ProfileManagement/models/automotive_shop_profile_model.dart
@@ -4,18 +4,23 @@ class AutomotiveProfileModel {
   final String location;
   final String coverImage;
   final String profileImage;
-  // final List<String> dayOfTheWeek;
+  final List<String> daysOfTheWeek;
+  final String operationTime;
+  final List<String> serviceSpecialization; // New field
 
-
+  // Constructor for creating an instance of AutomotiveProfileModel
   AutomotiveProfileModel({
     required this.uid,
     required this.shopName,
     required this.location,
     required this.coverImage,
     required this.profileImage,
-    // required this.dayOfTheWeek,
+    required this.daysOfTheWeek,
+    required this.operationTime,
+    required this.serviceSpecialization, // Include new field in constructor
   });
 
+  // Convert the model to a map for storage or transfer
   Map<String, dynamic> toMap() {
     return {
       'uid': uid,
@@ -23,18 +28,23 @@ class AutomotiveProfileModel {
       'location': location,
       'coverImage': coverImage,
       'profileImage': profileImage,
-      // 'dayOfTheWeek': dayOfTheWeek,
+      'daysOfTheWeek': daysOfTheWeek,
+      'operationTime': operationTime,
+      'serviceSpecialization': serviceSpecialization, // Convert list to map
     };
   }
 
+  // Create a model instance from a document snapshot
   factory AutomotiveProfileModel.fromDocument(Map<String, dynamic> doc, String uid) {
     return AutomotiveProfileModel(
       uid: uid,
-      shopName: doc['shopName'] ?? '',
-      location: doc['location'] ?? '',
-      coverImage: doc['coverImage'] ?? '',
-      profileImage: doc['profileImage'] ?? '',
-      // dayOfTheWeek: List<String>.from(doc['dayOfTheWeek'] ?? []),
+      shopName: doc['shopName'] as String? ?? '',
+      location: doc['location'] as String? ?? '',
+      coverImage: doc['coverImage'] as String? ?? '',
+      profileImage: doc['profileImage'] as String? ?? '',
+      daysOfTheWeek: List<String>.from(doc['daysOfTheWeek'] ?? []),
+      operationTime: doc['operationTime'] as String? ?? '',
+      serviceSpecialization: List<String>.from(doc['serviceSpecialization'] ?? []), // Convert to List<String>
     );
   }
 }
