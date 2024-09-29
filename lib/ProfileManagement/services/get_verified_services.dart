5afb62886a9ba354d50545f9b9f65a0534f1cd26
candace1818 <candacehyacinth.yu-18@cpu.edu.ph>
2024-09-29 10:54:26 +0800

    Updated Get Verification services

--- lib/ProfileManagement/services/get_verified_services.dart
+++ lib/ProfileManagement/services/get_verified_services.dart
@@ -1,8 +1,15 @@
+import 'package:autocare_automotiveshops/ProfileManagement/services/profile_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:intl/intl.dart';
 import 'dart:io';
 
+import '../models/automotive_shop_getVerified_model.dart';
+import '../models/automotive_shop_profile_model.dart';
+
 class GetVerifiedServices {
+  final ProfileService _profileService = ProfileService();
   // Method to pick and upload file
   Future<String?> pickAndUploadFile() async {
     // Pick a file
@@ -17,7 +24,7 @@ class GetVerifiedServices {
 
       // Upload the file to Firebase Storage
       try {
-        Reference storageRef = FirebaseStorage.instance.ref().child('uploads/$fileName');
+        Reference storageRef = FirebaseStorage.instance.ref().child('getVerified/$fileName');
         UploadTask uploadTask = storageRef.putFile(file);
 
         TaskSnapshot taskSnapshot = await uploadTask;
@@ -29,6 +36,34 @@ class GetVerifiedServices {
       }
     } else {
       return null; // No file selected
+    }
+  }
+
+  Future<void> saveVerificationData(String fileUrl) async {
+    try {
+      // Fetch the profile data
+      AutomotiveProfileModel? profile = await _profileService.fetchProfileData();
+
+      if (profile != null) {
+        final verificationData = VerificationModel(
+          uid: profile.uid,
+          shopName: profile.shopName,
+          location: profile.location,
+          dateSubmitted: DateFormat('yyyy-MM-dd').format(DateTime.now()),
+          timeSubmitted: DateFormat('HH:mm').format(DateTime.now()),
+          fileUrl: fileUrl,
+          status: 'pending',
+        );
+
+        await FirebaseFirestore.instance
+            .collection('verificationData')
+            .doc(profile.uid) // Use the user's UID
+            .set(verificationData.toMap());
+      } else {
+        throw Exception('Profile data not found');
+      }
+    } catch (e) {
+      print('Failed to save verification data: $e');
     }
   }
 }
