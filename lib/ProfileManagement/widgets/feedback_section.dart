17c8ede4a33c9678139aa8d9d4bab65ea4fe2393
PaulVincentLerado17 <104497476+PaulVincentLerado17@users.noreply.github.com>
2024-09-29 17:05:59 +0800

    update: feedback section widget

--- lib/ProfileManagement/widgets/feedback_section.dart
+++ lib/ProfileManagement/widgets/feedback_section.dart
@@ -10,26 +10,43 @@ class FeedbackSection extends StatelessWidget {
       children: [
         const Padding(
           padding: EdgeInsets.all(8.0),
-          child: Text('Feedbacks', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-        ),
-        Container(
-          margin: const EdgeInsets.all(8),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(16),
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              'Feedbacks',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
           ),
-          child: const Padding(
-            padding: EdgeInsets.all(18.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('Paul Vincent Lerado', style: TextStyle(fontWeight: FontWeight.bold)),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 10),
-                  child: Text('I was impressed with the professionalism and efficiency of your team during my recent oil change and brake inspection. '
-                      'However, the service took longer than expected, so providing more accurate time estimates would be helpful.'),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: PhysicalModel(
+            color: Colors.white,
+            elevation: 4, // Adds elevation to the container
+            borderRadius: BorderRadius.circular(16), // Matches the container's borderRadius
+            shadowColor: Colors.grey,
+            child: Container(
+              margin: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Paul Vincent Lerado', style: TextStyle(fontWeight: FontWeight.bold)),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 10),
+                      child: Text(
+                        'I was impressed with the professionalism and efficiency of your team during my recent oil change and brake inspection. '
+                            'However, the service took longer than expected, so providing more accurate time estimates would be helpful.',
+                      ),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
           ),
         ),
