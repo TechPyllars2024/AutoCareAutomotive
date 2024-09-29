ec2c726abe570f31f1785fd081bdfb31e9fd9106
PaulVincentLerado17 <104497476+PaulVincentLerado17@users.noreply.github.com>
2024-09-29 20:23:09 +0800

    update: field for text widget

--- lib/Authentication/widgets/text_field.dart
+++ lib/Authentication/widgets/text_field.dart
@@ -28,39 +28,45 @@ class _TextFieldInputState extends State<TextFieldInput> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+      padding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          TextField(
-            style: const TextStyle(fontSize: 15),
-            controller: widget.textEditingController,
-            obscureText: widget.isPass, // Handle password visibility
-            keyboardType: widget.textInputType,
-            decoration: InputDecoration(
-              labelText: widget.hintText,
-              prefixIcon: widget.icon != null
-                  ? Icon(widget.icon, color: Colors.black, size: 24)
-                  : null,
-              labelStyle:
-                  const TextStyle(color: Color.fromARGB(255, 77, 76, 76)),
-              border: const OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.black),
+          SizedBox(
+            height: 45,
+            child: TextField(
+              style: const TextStyle(fontSize: 15),
+              controller: widget.textEditingController,
+              obscureText: widget.isPass, // Handle password visibility
+              keyboardType: widget.textInputType,
+              decoration: InputDecoration(
+                labelText: widget.hintText,
+                prefixIcon: widget.icon != null
+                    ? Icon(widget.icon, color: Colors.grey.shade800, size: 18)
+                    : null,
+                labelStyle:
+                const TextStyle(color: Color.fromARGB(255, 77, 76, 76)),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                  borderSide: BorderSide(color: Colors.grey),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                  borderSide: BorderSide(color: Colors.grey),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                  borderSide: BorderSide(color: Colors.grey),
+                ),
+                // Remove errorText from InputDecoration
               ),
-              enabledBorder: const OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.black),
-              ),
-              focusedBorder: const OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.black),
-              ),
-              // Remove errorText from InputDecoration
+              onChanged: (value) {
+                setState(() {
+                  errorMessage =
+                      widget.validator?.call(value); // Validate on text change
+                });
+              },
             ),
-            onChanged: (value) {
-              setState(() {
-                errorMessage =
-                    widget.validator?.call(value); // Validate on text change
-              });
-            },
           ),
           if (errorMessage != null && errorMessage!.isNotEmpty)
             Padding(
