d81c835b6d1c9f435ec209540b88695853ee1e14
RafaelIIIPrudente <97773959+RafaelIIIPrudente@users.noreply.github.com>
2024-09-30 19:50:43 +0800

    feature-feedback_management: working feedback management that appears in the profile

--- lib/ProfileManagement/screens/automotive_profile.dart
+++ lib/ProfileManagement/screens/automotive_profile.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:autocare_automotiveshops/ProfileManagement/widgets/top_section.dart';
 import 'package:autocare_automotiveshops/ProfileManagement/widgets/profile_details.dart';
 import 'package:autocare_automotiveshops/ProfileManagement/widgets/services_carousel.dart';
+import 'package:flutter_pannable_rating_bar/flutter_pannable_rating_bar.dart';
 import '../models/feedbacks_model.dart';
 import '../services/profile_service.dart';
 import '../models/automotive_shop_profile_model.dart';
@@ -26,11 +27,14 @@ class _AutomotiveProfileScreenState extends State<AutomotiveProfileScreen> {
 
   final double coverHeight = 160;
   final double profileHeight = 100;
+  late Future<Map<String, dynamic>> _providerData;
 
   @override
   void initState() {
     super.initState();
     _loadProfileData();
+    _providerData =
+        ProfileService().fetchProviderByUid(user!.uid);
   }
 
   Future<void> _loadProfileData() async {
@@ -66,6 +70,7 @@ class _AutomotiveProfileScreenState extends State<AutomotiveProfileScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final double top = coverHeight - profileHeight / 2;
     return Scaffold(
       backgroundColor: Colors.grey.shade100,
       appBar: AppBar(
@@ -76,27 +81,103 @@ class _AutomotiveProfileScreenState extends State<AutomotiveProfileScreen> {
         backgroundColor: Colors.grey.shade300,
         foregroundColor: Colors.black,
       ),
-      body: SafeArea(
-        child: ListView(
-          children: [
-            TopSection(
-              coverHeight: coverHeight,
-              profileHeight: profileHeight,
-              profile: profile,
-            ),
-            ProfileDetails(profile: profile),
-            buildButton(),
-            const SizedBox(height: 10),
-            buildGetVerified(),
-            const SizedBox(height: 10),
-            buildCheckStatus(),
-            const ServicesCarousel(),
-            feedbackSection(user?.uid ?? ''),
-          ],
-        ),
+      body: FutureBuilder<Map<String, dynamic>>(
+        future: _providerData,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data == null) {
+            return const Center(child: Text('No data available.'));
+          } else {
+            final data = snapshot.data!;
+            return ListView(
+              children: [
+                buildTopSection(data, top),
+                ProfileDetails(profile: profile),
+                buildButton(),
+                const SizedBox(height: 10),
+                buildGetVerified(),
+                const SizedBox(height: 10),
+                buildCheckStatus(),
+                const ServicesCarousel(),
+                feedbackSection(user?.uid ?? ''),
+              ],
+            );
+          }
+        }
       ),
     );
   }
+
+  Widget buildTopSection(Map<String, dynamic> data, double top) {
+    double rating =
+        data['totalRatings'] ?? 0;
+    int numberOfRating =
+        data['numberOfRatings'] ?? 0;
+
+    return Stack(
+      clipBehavior: Clip.none,
+      alignment: Alignment.center,
+      children: [
+        Container(
+          margin: EdgeInsets.only(bottom: profileHeight / 2),
+          child: buildCoverImage(data),
+        ),
+        Positioned(
+          left: 20,
+          top: top,
+          child: buildProfileImage(data),
+        ),
+        Positioned(
+          right: 20,
+          top: coverHeight + 10,
+          child: Row(
+            children: [
+              PannableRatingBar(
+                rate: rating,
+                items: List.generate(
+                  5,
+                      (index) =>  RatingWidget(
+                    selectedColor: Colors.orange.shade900,
+                    unSelectedColor: Colors.grey,
+                    child: const Icon(
+                      Icons.star,
+                      size: 15,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(width: 5),
+              Text(
+                '$numberOfRating ratings',
+                style:
+                const TextStyle(fontWeight: FontWeight.bold, fontSize: 13),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget buildCoverImage(Map<String, dynamic> data) => Container(
+    color: Colors.grey,
+    child: Image.network(
+      data['coverImage'] ?? 'default_cover_image_url',
+      width: double.infinity,
+      height: coverHeight,
+      fit: BoxFit.cover,
+    ),
+  );
+
+  Widget buildProfileImage(Map<String, dynamic> data) => CircleAvatar(
+    radius: profileHeight / 2,
+    backgroundColor: Colors.grey.shade800,
+    backgroundImage:
+    NetworkImage(data['profileImage'] ?? 'default_profile_image_url'),
+  );
 
   Widget buildButton() => WideButtons(
     onTap: editProfile,
