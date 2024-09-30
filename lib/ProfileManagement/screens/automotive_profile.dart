b57566b2e94b61a18f6f88fd31a5feed7a4833ec
RafaelIIIPrudente <97773959+RafaelIIIPrudente@users.noreply.github.com>
2024-09-30 19:22:07 +0800

    feature-feedback_management: working feedback management that appears in the profile

--- lib/ProfileManagement/screens/automotive_profile.dart
+++ lib/ProfileManagement/screens/automotive_profile.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:autocare_automotiveshops/ProfileManagement/widgets/top_section.dart';
 import 'package:autocare_automotiveshops/ProfileManagement/widgets/profile_details.dart';
 import 'package:autocare_automotiveshops/ProfileManagement/widgets/services_carousel.dart';
-import 'package:autocare_automotiveshops/ProfileManagement/widgets/feedback_section.dart';
+import '../models/feedbacks_model.dart';
 import '../services/profile_service.dart';
 import '../models/automotive_shop_profile_model.dart';
 import '../widgets/button.dart'; // Assuming this is the button widget file
@@ -22,6 +22,7 @@ class _AutomotiveProfileScreenState extends State<AutomotiveProfileScreen> {
   final ProfileService _profileService = ProfileService();
   AutomotiveProfileModel? profile;
   final user = FirebaseAuth.instance.currentUser;
+  bool isExpanded = false;
 
   final double coverHeight = 160;
   final double profileHeight = 100;
@@ -90,7 +91,7 @@ class _AutomotiveProfileScreenState extends State<AutomotiveProfileScreen> {
             const SizedBox(height: 10),
             buildCheckStatus(),
             const ServicesCarousel(),
-            const FeedbackSection(),
+            feedbackSection(user?.uid ?? ''),
           ],
         ),
       ),
@@ -120,4 +121,153 @@ class _AutomotiveProfileScreenState extends State<AutomotiveProfileScreen> {
     onTap: checkStatus,
     text: 'Check Status',
   );
+
+  Widget feedbackSection(String serviceProviderUid) =>
+      StreamBuilder<List<FeedbackModel>>(
+      stream: ProfileService().fetchFeedbacks(serviceProviderUid),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+          return const Center(child: Text('No feedbacks available.'));
+        } else {
+          final feedbacks = snapshot.data!;
+
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Feedback',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 150,
+
+                child: PageView.builder(
+                  controller: PageController(viewportFraction: 0.85),
+                  itemCount: feedbacks.length,
+                  itemBuilder: (context, index) {
+                    final feedback = feedbacks[index];
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: PhysicalModel(
+                        color: Colors.white,
+                        elevation: 5,
+                        shadowColor: Colors.grey,
+                        borderRadius: BorderRadius.circular(15),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            color: Colors.white,
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(12.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  children: [
+                                    CircleAvatar(
+                                      radius: 16,
+                                      backgroundColor: Colors.blueGrey[50],
+                                      child: Text(
+                                        feedback.feedbackerName[0], // First letter of the feedbacker's name
+                                        style: const TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 16,
+                                          color: Colors.black87,
+                                        ),
+                                      ),
+                                    ),
+                                    const SizedBox(width: 10),
+                                    Expanded(
+                                      child: Text(
+                                        feedback.feedbackerName,
+                                        style: const TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 15,
+                                          color: Colors.black87,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                const SizedBox(height: 8),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    GestureDetector(
+                                      onTap: () {
+                                        setState(() {
+                                          isExpanded = !isExpanded;
+                                        });
+                                      },
+                                      child: Text(
+                                        feedback.comment,
+                                        style: TextStyle(
+                                          fontSize: isExpanded ? 12 : 13, // Decrease font size if expanded
+                                          color: Colors.black54,
+                                        ),
+                                        overflow: isExpanded ? TextOverflow.visible : TextOverflow.ellipsis,
+                                        maxLines: isExpanded ? null : 2, // Show all lines if expanded
+                                        softWrap: true,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+
+
+                                const Spacer(),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Icon(Icons.star, color: Colors.orange.shade900, size: 16),
+                                        const SizedBox(width: 5),
+                                        Text(
+                                          feedback.rating.toString(),
+                                          style: const TextStyle(
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    Text(
+                                      _formatTimestamp(feedback.timestamp), // Function to format timestamp
+                                      style: const TextStyle(
+                                        fontSize: 12,
+                                        color: Colors.black45,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          );
+        }
+      },
+    );
+
+// Helper function to format the timestamp
+  String _formatTimestamp(DateTime timestamp) {
+    return '${timestamp.day}/${timestamp.month}/${timestamp.year}';
+  }
 }
