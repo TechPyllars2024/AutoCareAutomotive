4dedc60893d5f85b6c0722f0c3df30fecd32f25b
candace1818 <candacehyacinth.yu-18@cpu.edu.ph>
2024-10-05 19:16:11 +0800

    Updated Main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:autocare_automotiveshops/ProfileManagement/screens/automotive_main_profile.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
