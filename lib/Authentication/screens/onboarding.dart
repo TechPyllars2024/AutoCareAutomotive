ea12fa4ec931d1323c6a047d8b4d6f2d73c1123d
PaulVincentLerado17 <104497476+PaulVincentLerado17@users.noreply.github.com>
2024-10-06 14:13:11 +0800

    update: main onboarding

--- lib/Authentication/screens/onboarding.dart
+++ lib/Authentication/screens/onboarding.dart
@@ -1,15 +1,10 @@
 import 'package:autocare_automotiveshops/Authentication/screens/onboardingPage1.dart';
 import 'package:autocare_automotiveshops/Authentication/screens/onboardingPage2.dart';
 import 'package:autocare_automotiveshops/Authentication/screens/onboardingPage3.dart';
-import 'package:autocare_automotiveshops/Navigation Bar/navbar.dart';
-
-import 'package:flutter/foundation.dart';
+import 'package:autocare_automotiveshops/Navigation%20Bar/navbar.dart';
 import 'package:flutter/material.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
-
 import '../../ProfileManagement/screens/automotive_edit_profile.dart';
-
-
 
 class Onboarding extends StatefulWidget {
   const Onboarding({super.key});
@@ -19,75 +14,79 @@ class Onboarding extends StatefulWidget {
 }
 
 class _OnboardingState extends State<Onboarding> {
-
   PageController _controller = PageController();
-
-
   bool onLastPage = false;
-  bool onFirstPage = false;
-
+  bool onFirstPage = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
-        children: [ PageView(
-
-          controller: _controller,
-
-          onPageChanged: (index) {
-            setState(() {
-              onLastPage = (index == 3);
-            });
-          },
-          children: [
-            Onboardingpage1(),
-            Onboardingpage2(),
-            AutomotiveEditProfile(),
-            Onboardingpage3(),
-
-        
-          ],
-        ),
+        children: [
+          PageView(
+            controller: _controller,
+            onPageChanged: (index) {
+              setState(() {
+                onLastPage = (index == 3);
+                onFirstPage = (index == 0);
+              });
+            },
+            children: [
+              Onboardingpage1(),
+              Onboardingpage2(),
+              AutomotiveEditProfile(),
+              Onboardingpage3(),
+            ],
+          ),
           Container(
             alignment: Alignment(0, 0.9),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-
-                  GestureDetector(
-                      onTap: () {
-                        _controller.previousPage(duration: Duration(microseconds: 500), curve: Curves.easeIn);
-                      },
-                      child: Text("previous")),
-
-                  SmoothPageIndicator(controller: _controller, count: 4,
-                    effect: ExpandingDotsEffect(
-                    dotColor: Colors.grey,  // Inactive dot color
-                    activeDotColor: Colors.orange.shade900,  // Active dot color
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                onFirstPage
+                    ? Text("      ") // Don't show "pre" if on first page
+                    : GestureDetector(
+                  onTap: () {
+                    _controller.previousPage(
+                        duration: Duration(milliseconds: 500),
+                        curve: Curves.easeIn);
+                  },
+                  child: Icon(Icons.navigate_before),
+                ),
+                SmoothPageIndicator(
+                  controller: _controller,
+                  count: 4,
+                  effect: ExpandingDotsEffect(
+                    dotColor: Colors.grey, // Inactive dot color
+                    activeDotColor: Colors.orange.shade900, // Active dot color
                     dotHeight: 8.0,
                     dotWidth: 8.0,
                   ),
-
-                  ),
-
-                  onLastPage?
-                  GestureDetector(
-                      onTap: () {
-                        Navigator.push(context, MaterialPageRoute(builder: (context) => Navbar()));
-                      },
-
-                      child: Text("")):
-                  GestureDetector(
-                      onTap: () {
-                        _controller.nextPage(duration: Duration(microseconds: 500), curve: Curves.easeIn);
-                      },
-
-                      child: Text("next")),
-                ],
-              ))
-    ]
-      )
+                ),
+                onLastPage
+                    ? GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => Navbar()),
+                    );
+                  },
+                  child: Text(""),
+                )
+                    : GestureDetector(
+                  onTap: () {
+                    _controller.nextPage(
+                        duration: Duration(milliseconds: 500),
+                        curve: Curves.easeIn);
+                  },
+                  child: Icon(Icons.navigate_next),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
