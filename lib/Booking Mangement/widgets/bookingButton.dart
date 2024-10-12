d0e5848a00eea1d511576267fcc8928366f9a95e
PaulVincentLerado17 <104497476+PaulVincentLerado17@users.noreply.github.com>
2024-10-12 17:51:26 +0800

    update: code clean up - booking button

--- lib/Booking Mangement/widgets/bookingButton.dart
+++ lib/Booking Mangement/widgets/bookingButton.dart
@@ -1,32 +1,32 @@
 import 'package:flutter/material.dart';
 
 class BookingButton extends StatelessWidget {
-  final VoidCallback? onTap; // Nullable onTap callback
+  final VoidCallback? onTap;
   final String text;
   final Color color;
   final double padding;
-  final double width; // Add a width parameter
+  final double width;
 
   const BookingButton({
     super.key,
-    this.onTap, // Optional onTap callback
+    this.onTap,
     required this.text,
     this.color = Colors.orange,
     this.padding = 15.0,
-    this.width = 140.0, // Default width value
+    this.width = 140.0,
   });
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: onTap ?? () {}, // Use an empty function if onTap is null
+      onTap: onTap ?? () {},
       splashColor: Colors.black26,
       child: Padding(
         padding: EdgeInsets.all(padding),
         child: Container(
           alignment: Alignment.center,
           padding: const EdgeInsets.symmetric(vertical: 12),
-          width: width, // Apply width here
+          width: width,
           decoration: ShapeDecoration(
             shape: const RoundedRectangleBorder(
               borderRadius: BorderRadius.all(
