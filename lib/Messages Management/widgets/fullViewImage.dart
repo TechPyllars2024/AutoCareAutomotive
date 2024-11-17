797fa259bf15d542e538666a4b175b4ba1b461d8
candace1818 <candacehyacinth.yu-18@cpu.edu.ph>
2024-11-17 21:46:50 +0800

    Added full view image widget

---
+++ lib/Messages Management/widgets/fullViewImage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class FullImageView extends StatelessWidget {
+  final String imageUrl;
+
+  const FullImageView({required this.imageUrl, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.orange.shade900,
+        iconTheme: const IconThemeData(color: Colors.white),
+      ),
+      body: Center(
+        child: InteractiveViewer(
+          child: Image.network(imageUrl),
+        ),
+      ),
+    );
+  }
+}
