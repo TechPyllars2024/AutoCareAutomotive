42e226cdcab5d972951abd167f75c759ce4394e8
RafaelIIIPrudente <97773959+RafaelIIIPrudente@users.noreply.github.com>
2024-12-12 15:25:32 +0800

    fix-commission: updated and fixes the automotive_commission.dart

--- lib/ProfileManagement/screens/automotive_commission.dart
+++ lib/ProfileManagement/screens/automotive_commission.dart
@@ -6,6 +6,7 @@ import '../services/commission_services.dart';
 
 class AutomotiveCommission extends StatefulWidget {
   final serviceProviderUid;
+
   const AutomotiveCommission({super.key, required this.serviceProviderUid});
 
   @override
@@ -13,29 +14,35 @@ class AutomotiveCommission extends StatefulWidget {
 }
 
 class _AutomotiveCommissionState extends State<AutomotiveCommission> {
-  double totalCommission = 0.0;
+  Map<String, double>? totalCommission;
   List<Commission> commissionDetails = [];
+  bool isLoading = true; // Track loading state
 
   @override
   void initState() {
     super.initState();
-    _calculateTotalCommission();
     _loadCommissionData();
+    _calculateTotalCommission();
   }
 
   Future<void> _calculateTotalCommission() async {
-    final total = await CommissionService.calculateTotalCommission();
+    final total = await CommissionService.calculateTotalCommissionByProvider();
     setState(() {
       totalCommission = total;
     });
   }
 
   Future<void> _loadCommissionData() async {
-    final commissions = await CommissionService.fetchCommissionDetails(widget.serviceProviderUid);
-    final total = await CommissionService.calculateTotalCommission();
+    setState(() {
+      isLoading = true; // Set loading state to true before fetching data
+    });
+
+    final commissions = await CommissionService.fetchCommissionDetails(
+        widget.serviceProviderUid);
+
     setState(() {
       commissionDetails = commissions;
-      totalCommission = total;
+      isLoading = false;
     });
   }
 
@@ -57,146 +64,159 @@ class _AutomotiveCommissionState extends State<AutomotiveCommission> {
           },
         ),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Container(
-              height: 120,
-              decoration: BoxDecoration(
-                color: Colors.orange.shade900,
-                borderRadius: const BorderRadius.only(
-                  bottomLeft: Radius.circular(20),
-                  bottomRight: Radius.circular(20),
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.3),
-                    spreadRadius: 2,
-                    blurRadius: 30,
-                    offset: const Offset(0, 5),
-                  ),
-                ],
-              ),
-              padding: const EdgeInsets.all(10),
-              width: double.infinity,
-              alignment: Alignment.centerLeft,
-              child: Padding(
-                padding: const EdgeInsets.all(4.0),
-                child: Row(
-                  children: [
-                    Column(
-                      children: [
-                        const Text(
-                          'Total',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 20.0),
-                          child: Text(
-                            '₱ ${totalCommission.toStringAsFixed(2)}',
-                            style: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 40,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
+      body: isLoading
+          ? const Center(
+              child: CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.orange),
+              ), // Show loading spinner
+            )
+          : SingleChildScrollView(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Container(
+                    height: 120,
+                    decoration: BoxDecoration(
+                      color: Colors.orange.shade900,
+                      borderRadius: const BorderRadius.only(
+                        bottomLeft: Radius.circular(20),
+                        bottomRight: Radius.circular(20),
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.3),
+                          spreadRadius: 2,
+                          blurRadius: 30,
+                          offset: const Offset(0, 5),
                         ),
                       ],
                     ),
-                    const Spacer(),
-                    const Text(
-                      '2% of every booking',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 14,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            const SizedBox(height: 30),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 5),
-              child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                "Commission Details",
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              )),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Column(
-                children: List.generate(commissionDetails.length, (index) {
-                  final commission = commissionDetails[index];
-                  return Card(
-                    color: Colors.white,
-                    elevation: 10,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12), // Rounded corners for the card
-                    ),
-                    margin: const EdgeInsets.only(bottom: 16), // Margin between cards
+                    padding: const EdgeInsets.all(10),
+                    width: double.infinity,
+                    alignment: Alignment.centerLeft,
                     child: Padding(
-                      padding: const EdgeInsets.all(16),
-                      child: Column( // Use Column for better layout management
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                      padding: const EdgeInsets.all(4.0),
+                      child: Row(
                         children: [
-                          // Car Owner and Service Name (First Column)
-                          Text(
-                            commission.carOwnerName,
-                            style: const TextStyle(
-                              fontSize: 15,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          const SizedBox(height: 8), // Space between owner name and service name
-
-                          // Service Name (Second Column)
-                          Text(
-                            commission.serviceName,
-                            style: const TextStyle(fontSize: 13),
-                            overflow: TextOverflow.ellipsis,
-                            maxLines: 2,
-                          ),
-                          const SizedBox(height: 8),
-
-                          // Pricing (Third Column)
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          Column(
                             children: [
-                              Text(
-                                '₱ ${commission.totalPrice.toStringAsFixed(2)}',
-                                style: const TextStyle(
-                                  fontSize: 15,
+                              const Text(
+                                'Total',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
                                   fontWeight: FontWeight.bold,
                                 ),
                               ),
-                              Text(
-                                '+ ₱ ${commission.commissionAmount.toStringAsFixed(2)}',
-                                style: const TextStyle(
-                                  fontSize: 13,
-                                  color: Colors.green,
+                              Padding(
+                                padding: const EdgeInsets.only(left: 20.0),
+                                child: Text(
+                                  totalCommission != null
+                                      ? '₱ ${totalCommission?[widget.serviceProviderUid]}'
+                                      : '₱ 0',
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 40,
+                                    fontWeight: FontWeight.bold,
+                                  ),
                                 ),
                               ),
                             ],
                           ),
+                          const Spacer(),
+                          const Text(
+                            '2% of every booking\ngoes to AutoCare',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            textAlign: TextAlign.center,
+                          )
                         ],
                       ),
                     ),
-                  );
-                }),
+                  ),
+                  const SizedBox(height: 30),
+                  const Padding(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 15.0, vertical: 5),
+                    child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          "Commission Details",
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        )),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: Column(
+                      children:
+                          List.generate(commissionDetails.length, (index) {
+                        final commission = commissionDetails[index];
+                        return Card(
+                          color: Colors.white,
+                          elevation: 10,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          margin: const EdgeInsets.only(bottom: 16),
+                          child: Padding(
+                            padding: const EdgeInsets.all(16),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                // Car Owner and Service Name (First Column)
+                                Text(
+                                  commission.carOwnerName,
+                                  style: const TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                const SizedBox(height: 8),
+
+                                // Service Name (Second Column)
+                                Text(
+                                  commission.serviceName,
+                                  style: const TextStyle(fontSize: 13),
+                                  overflow: TextOverflow.ellipsis,
+                                  maxLines: 2,
+                                ),
+                                const SizedBox(height: 8),
+
+                                // Pricing (Third Column)
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      '₱ ${commission.totalPrice.toStringAsFixed(2)}',
+                                      style: const TextStyle(
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    Text(
+                                      '+ ₱ ${commission.commissionAmount.toStringAsFixed(2)}',
+                                      style: const TextStyle(
+                                        fontSize: 13,
+                                        color: Colors.green,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      }),
+                    ),
+                  )
+                ],
               ),
-            )
-          ],
-        ),
-      ),
+            ),
     );
   }
 }
